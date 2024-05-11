244f55528e79385f45e02bfe4064eaa8fc9e30ef
farhanScripts <farhan.dev06@gmail.com>
2024-05-12 00:02:39 +0700

    Compelete caching features without workbox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: 'RestoRound-V1',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,12 +1,33 @@
+import CacheHelper from './utils/cache-helper';
+import 'regenerator-runtime';
+
+const assetsToCache = [
+  './',
+  './images/icons/icon-72x72.png',
+  './images/icons/icon-96x96.png',
+  './images/icons/icon-128x128.png',
+  './images/icons/icon-144x144.png',
+  './images/icons/icon-152x152.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-384x384.png',
+  './images/icons/icon-512x512.png',
+  './images/heros/hero-image_2.jpg',
+  './favicon.png',
+  './index.html',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
 self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker...');
+  console.log([...assetsToCache]);
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker...');
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
